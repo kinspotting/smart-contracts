622b95262bc899b08947bdf17b619440a532d081
Dan Octavian <danoctavian91@gmail.com>
2020-11-17 15:11:03 +0800

    add full test for fork. voting does not pass currently

--- test/fork/payout-address.js
+++ test/fork/payout-address.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const { artifacts, run, web3 } = require('hardhat');
+const { artifacts, run, web3, network } = require('hardhat');
 
 const NXMaster = artifacts.require('NXMaster');
 const MemberRoles = artifacts.require('MemberRoles');
@@ -24,6 +24,37 @@ describe('payout address update', function () {
       nameToAddressMap[web3.utils.toAscii(contractsName[i])] = contractsAddress[i];
     }
 
+    const mr = await MemberRoles.at(nameToAddressMap['MR']);
+    const tk = await NXMToken.at(nameToAddressMap['NXMTOKEN']);
+    const gv = await Governance.at(nameToAddressMap['GV']);
+
+    console.log('Fetch board members..');
+    const members = await mr.members('1');
+    const boardMembers = members.memberArray;
+
+    console.log(boardMembers);
+
+    console.log('Impersonating..');
+    for (let i = 0; i < boardMembers.length; i++) {
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [boardMembers[i]]
+      });
+    }
+
     // TODO: complete fork test here
+
+    const proposalId = 113;
+
+    console.log('Submitting votes..');
+    for (let i = 3; i < boardMembers.length; i++) {
+      console.log(`Voting for Board member ${i} ${boardMembers[i]}...`);
+      await gv.submitVote(proposalId, 1, { from: boardMembers[i] });
+    }
+
+    const isOwnerAMember = await mr.isMember(boardMembers[0]);
+    console.log({
+      isOwnerAMember
+    });
   });
 });
